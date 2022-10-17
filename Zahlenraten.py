efb991f5f53061de41763b8927b589dc29835c30
Louisa Strunk <louisa@MBP-von-Louisa.office.5minds.de>
2022-10-17 08:45:34 +0200

    add name and attempts

--- Zahlenraten.py
+++ Zahlenraten.py
@@ -1,12 +1,17 @@
 import random
 
 num = random.randint(1, 20)
+attempts = 0
 
-print("Hallo Spieler! Ich denke an eine Zahl zwischen 1 und 20. Welche ist es?")
+print("Hallo, wie heißt du?")
+name = input()
+
+print("Hallo " + name + "! Ich denke an eine Zahl zwischen 1 und 20. Welche ist es?")
 
 while True:
 
     guess = int(input())
+    attempts += 1
 
     if guess < num:
         print("Dein Versuch ist zu niedrig. Rate erneut!")
@@ -15,5 +20,5 @@ while True:
         print("Dein Versuch ist zu hoch. Rate erneut!")
 
     if guess == num:
-        print("Gut gemacht! Du hast die richtige Zahl erraten")
+        print("Gut gemacht! Du hast die richtige Zahl nach nur " + str(attempts) + " Versuchen erraten")
         break
