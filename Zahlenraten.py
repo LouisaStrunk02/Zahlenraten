ddf11939f6ef83bf255af46518370729efc298a0
Louisa Strunk <louisa@MBP-von-Louisa.office.5minds.de>
2022-10-17 10:04:16 +0200

    add hints and decision of new game

--- Zahlenraten.py
+++ Zahlenraten.py
@@ -1,24 +1,58 @@
 import random
 
-num = random.randint(1, 20)
-attempts = 0
+game = True
 
-print("Hallo, wie heißt du?")
-name = input()
+while game == True:
+    num = random.randint(1, 100)
+    attempts = 0
 
-print("Hallo " + name + "! Ich denke an eine Zahl zwischen 1 und 20. Welche ist es?")
+    print("Hallo, wie heißt du?")
+    name = input()
 
-while True:
+    print("Hallo " + name + "! Ich denke an eine Zahl zwischen 1 und 100. Welche ist es?")
 
-    guess = int(input())
-    attempts += 1
+    while True:
 
-    if guess < num:
-        print("Dein Versuch ist zu niedrig. Rate erneut!")
+        guess = int(input())
 
-    if guess > num:
-        print("Dein Versuch ist zu hoch. Rate erneut!")
+        while guess < 1 or guess > 100:
+            print("Gib bitte eine Zahl zwischen 1 und 100 an :)")
+            break
 
-    if guess == num:
-        print("Gut gemacht! Du hast die richtige Zahl nach nur " + str(attempts) + " Versuchen erraten")
-        break
+        if guess < num and guess >= 1:
+
+            if num - guess <= 10:
+                print("Heiß")
+            else :
+                print("Kalt")
+
+            print("Dein Versuch ist zu niedrig. Rate erneut!")
+            attempts += 1
+
+        if guess > num and guess <= 100:
+            
+            if guess - num <= 10:
+                print("Heiß")
+            else :
+                print("Kalt")
+
+            print("Dein Versuch ist zu hoch. Rate erneut!")
+            attempts += 1
+
+        if guess == num:
+            print("Gut gemacht! Du hast die richtige Zahl nach nur " + str(attempts) + " Versuchen erraten")
+            print("Möchtest du noch einmal spielen? y/n")
+            again = input()
+            attempts += 1
+
+            while True:
+                if again == "y":
+                    game = True
+                    break
+                elif again == "n":
+                    game = False
+                    break
+                else:
+                    print("Bitte gib y für ja und n für nein ein")
+
+            break
